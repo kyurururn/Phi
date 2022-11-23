220e4aa37f307ad2d44a0a4063de0c3e49965f79
kyurururn <75336100+kyurururn@users.noreply.github.com>
2022-11-23 10:43:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let rr_z;
+let rr_z = 0;
 
 const permission_request = () => {
     if(DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === "function"){
         DeviceMotionEvent.requestPermission().then((result) => {
             if(result === "granted"){
                 window.addEventListener("devicemotion",gyro,false);
-                rr_z = 0;
             }
         });
     }
@@ -20,18 +19,18 @@ const permission_request = () => {
 }
 
 const move = (event) => {
-    /*
     let beta  = Math.floor(event.beta);
     let gamma = Math.floor(event.gamma);
     let alpha = Math.floor(event.alpha);
     document.getElementById("shape").style.transform = "rotateX(" + beta + "deg) rotateY(" + gamma + "deg) rotateZ(" + alpha + "deg)";
-    */
 }
 
 
 const gyro = (event) => {
     let rotate_z = event.rotationRate.alpha;
     let interval = event.interval / 1000;
+
+    document.getElementById("shape").innerHTML = String(rotate_z);
 
     rr_z += rotate_z * interval
 
