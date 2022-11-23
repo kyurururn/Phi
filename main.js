a7419468552f44a3b621064a8fd96af894e79b6b
kyurururn <75336100+kyurururn@users.noreply.github.com>
2022-11-23 11:16:45 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let rr_z = 0;
+let shape_rotate_z = 0;
 
 const permission_request = () => {
     if(DeviceOrientationEvent && DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === "function"){
@@ -19,23 +19,21 @@ const permission_request = () => {
 }
 
 const move = (event) => {
+    /*
     let beta  = Math.floor(event.beta);
     let gamma = Math.floor(event.gamma);
     let alpha = Math.floor(event.alpha);
     document.getElementById("shape").style.transform = "rotateX(" + beta + "deg) rotateY(" + gamma + "deg) rotateZ(" + alpha + "deg)";
+    */
 }
 
 
 const gyro = (event) => {
-    /*
     let rotate_z = event.rotationRate.gamma;
     let interval = event.interval;
 
-    document.getElementById("shape").innerHTML = String(interval);
-
-    rr_z += rotate_z * interval
+    shape_rotate_z_est += rotate_z * interval
+    shape_rotate_z = shape_rotate_z * 0.9 + shape_rotate_z_est * 0.1
 
     document.getElementById("shape").style.transform = "rotateZ(" + String(rr_z) + "deg)";
-    */
-
 }
